d90f1a36e5c57b09818707200305ad7900e54c3c
GiselyKC <gikavalli@gmail.com>
2022-07-20 19:25:59 -0300

    feat: criado o arquivo MyProvider e já foi realizado a importação do arquivo db.json

---
+++ home-broker-xp/src/context/MyProvider.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import Context from './MyContext';
+import ApiFake from '../db.json';
+
+function Provider({ children }) {
+  const [usuarios, setUsuarios] = useState(ApiFake.usuarios);
+  const [acoes, setAcoes] = useState(ApiFake.acoes);
+
+  const contextValue = useMemo(() => ({
+    usuarios,
+    setUsuarios,
+    acoes,
+    setAcoes,
+  }), [usuarios, acoes]);
+
+  return (
+    <Context.Provider value={contextValue}>
+      { children }
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
