7b6efdd774f555517fef8d4bf0c72809530d6321
GiselyKC <gikavalli@gmail.com>
2022-07-20 19:43:14 -0300

    feat: adicionado a rota para o arq. NotFount

--- home-broker-xp/src/App.jsx
+++ home-broker-xp/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
+import NotFound from './Pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
