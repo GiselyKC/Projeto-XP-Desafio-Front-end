4d9e1b714533579c8ed65ab716654f447d082812
GiselyKC <gikavalli@gmail.com>
2022-07-20 19:42:23 -0300

    feat: criado o arquivo NotFount e adicionado a informação que será renderizada

---
+++ home-broker-xp/src/Pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <main>
+      <div>
+        <h1>Erro 404... Não encontrado!</h1>
+      </div>
+    </main>
+  );
+}
+
+export default NotFound;
