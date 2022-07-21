6824ea5a7b163d61ab52c2d12ecf85ee14467337
GiselyKC <gikavalli@gmail.com>
2022-07-20 21:17:08 -0300

    feat: adicionado a rota ComprarOuVender no arquivo App

--- home-broker-xp/src/App.jsx
+++ home-broker-xp/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import ListaDeAcoes from './Pages/ListaDeAcoes';
+import ComprarOuVender from './Pages/ComprarOuVender';
 import NotFound from './Pages/NotFound';
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Login />} />
       <Route path="/lista-acoes" element={<ListaDeAcoes />} />
+      <Route path="/comprar-vender" element={<ComprarOuVender />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
