2cf1289c80936bc930f28b091aee992cffc90e87
GiselyKC <gikavalli@gmail.com>
2022-07-22 14:50:37 -0300

    feat: criada a logica de deposito ou retirada, e também feita a importação do arquivo de css na pagina DepositoOuRetirada

--- home-broker-xp/src/Pages/DepositoOuRetirada.jsx
+++ home-broker-xp/src/Pages/DepositoOuRetirada.jsx
@@ -1,27 +1,46 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NomeUsuario from '../components/NomeUsuario';
 import Context from '../context/MyContext';
+import '../css/depositoOuRetirada.css';
 
 export default function DepositoOuRetirada() {
   const navigate = useNavigate();
   const { email, usuarios } = useContext(Context);
-  const [valor, setValor] = useState(0);
   const [valueBotao, setValueBotao] = useState('');
+  const [inputValor, setInputValor] = useState('');
+  const [contaValor, setContaValor] = useState(0);
 
-  const filterConta = usuarios.filter((usuario) => usuario.email === email)
-    .map(({ conta }) => conta);
+  useEffect(() => {
+    usuarios.filter((usuario) => usuario.email === email)
+      .map(({ conta }) => setContaValor(conta));
+  }, []);
+
+  const botaoConfirmar = () => {
+    if (valueBotao === 'botaoDeposito') {
+      setContaValor(contaValor + Number(inputValor));
+    } if (valueBotao === 'botaoRetirada') {
+      setContaValor(contaValor - Number(inputValor));
+    } if (valueBotao === '') {
+      alert('Deve selecionar as opções de Deposito ou Retirada');
+    } if (contaValor <= 0 && valueBotao === 'botaoRetirada') {
+      alert('Saldo insuficiente para retirada');
+      setContaValor(0);
+    }
+    return botaoConfirmar;
+  };
 
   return (
-    <div>
-      <h1>DepositoOuRetirada</h1>
+    <div className="container-deposito-retirada">
+      {/* <h1>DepositoOuRetirada</h1> */}
       <NomeUsuario />
-      <div>
+      <div className="container-saldo">
         <p>Saldo em Conta:</p>
-        <p>{`R$ ${filterConta}`}</p>
+        <p>{`R$ ${contaValor}`}</p>
       </div>
-      <div>
+      <div className="container-btn-deposito-retirada">
         <button
+          className="btn-deposito-retirada"
           type="button"
           value="botaoDeposito"
           onClick={({ target: { value } }) => setValueBotao(value)}
@@ -29,6 +48,7 @@ export default function DepositoOuRetirada() {
           Depósito
         </button>
         <button
+          className="btn-deposito-retirada"
           type="button"
           value="botaoRetirada"
           onClick={({ target: { value } }) => setValueBotao(value)}
@@ -37,19 +57,22 @@ export default function DepositoOuRetirada() {
         </button>
       </div>
       <input
+        className="input-deposito-retirada"
         type="text"
         id="text"
         placeholder="Informe o Valor"
-        onChange={({ target: { value } }) => setValor(value)}
+        onChange={({ target: { value } }) => setInputValor(value)}
       />
-      <div>
+      <div className="container-btn-deposito-retirada">
         <button
+          className="btn-voltar-confirmar"
           type="button"
           onClick={() => navigate('/lista-acoes')}
         >
           Voltar
         </button>
         <button
+          className="btn-voltar-confirmar"
           type="button"
           onClick={botaoConfirmar}
         >
