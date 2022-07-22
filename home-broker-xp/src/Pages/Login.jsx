cc55ed104887151d4dddfeb94ddd4a788ae1036d
GiselyKC <gikavalli@gmail.com>
2022-07-22 14:53:32 -0300

    feat: criada a logica para verificar o usuario na pagina de login

--- home-broker-xp/src/Pages/Login.jsx
+++ home-broker-xp/src/Pages/Login.jsx
@@ -1,32 +1,50 @@
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Context from '../context/MyContext';
+import '../css/login.css';
 
 export default function Login() {
   const navigate = useNavigate();
-  const { setEmail } = useContext(Context);
-  const [senha, setSenha] = useState('');
+  const { email, setEmail, usuarios } = useContext(Context);
+  const [senhaLogin, setSenhaLogin] = useState('');
 
-  const redirect = () => {
-    navigate('/lista-acoes');
+  const valueSenha = ({ target: { value } }) => {
+    setSenhaLogin(value);
+  };
+
+  const habilitarBotao = () => {
+    const teste = usuarios.find((usuario) => usuario.email === email).senha;
+    if (senhaLogin !== teste) {
+      alert('Senha incorreta');
+    }
+    if (senhaLogin === teste) {
+      navigate('/lista-acoes');
+    }
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <input
-        type="text"
-        name="email"
-        placeholder="email"
-        onChange={({ target: { value } }) => setEmail(value)}
-      />
-      <input
-        type="password"
-        name="senha"
-        placeholder="senha"
-        onChange={({ target: { value } }) => setSenha({ senha: value })}
-      />
-      <button type="button" onClick={redirect}>
+    <div className="container-login">
+      <div className="container-input-login">
+        <input
+          className="input-login"
+          type="text"
+          name="email"
+          placeholder="email"
+          onChange={({ target: { value } }) => setEmail(value)}
+        />
+        <input
+          className="input-login"
+          type="password"
+          name="senha"
+          placeholder="senha"
+          onChange={valueSenha}
+        />
+      </div>
+      <button
+        type="button"
+        className="btn-login"
+        onClick={habilitarBotao}
+      >
         Acessar
       </button>
     </div>
